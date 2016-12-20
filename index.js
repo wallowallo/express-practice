1c8c81d00008662853cfeccc5f644666d7c22937
Krister <krister.torsvik@gmail.com>
2016-12-20 21:39:52 +0100

    need mock data for users

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express')
 var app = express()
 
+var fs = require('fs')
+var _ = require('lodash')
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function(err) {
+  if(err) throw err
+  
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+})
+
 app.get('/', function (req, res) {
   res.send('hello, world')
 })
